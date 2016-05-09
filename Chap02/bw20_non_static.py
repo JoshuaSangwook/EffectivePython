e13cc614366754ecf276d68591158168d1bc3235
ianstream <ianstream@naver.com>
2016-05-10 00:28:17 +0900

    5장 concurrent

--- Chap02/bw20_non_static.py
+++ Chap02/bw20_non_static.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from time import sleep
 
@@ -10,7 +11,7 @@ log('hello world!')
 sleep(0.5)
 log('hello again!')
 
-# 기본값을 None 으로 하고 문서화 문자열을 추가해준다
+# 동적 기본인수를 위해 기본값을 None 으로 하고 docstring(문서화 문자열)을 추가해준다
 def log(message, when=None):
     """
     시간과 문자열을 출력해준다
@@ -25,5 +26,54 @@ def log(message, when=None):
 log('hello world!')
 sleep(0.5)
 log('hello again!')
+
+
+
+
+
+def decode(data, default={}):
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+foo = decode('bad data')
+foo['stuff'] = 5
+
+bar = decode('also bad')
+bar['meep'] = 1
+
+print('foo:', foo)
+print('bar:', bar)
+
+# 다르게 나올거라 기대했겠지만, 결과는 같다. 키워드 인수의 기본값을 None 으로 설정하고 동작을 문서화하자
+
+def decode(data, default=None):
+    """Load json data from a string
+
+    args:
+        data: json data to decode
+        default: value to return if decofing fails
+            defaults to am empty dictionary
+    """
+    if default is None:
+        default = {}
+
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+
+foo = decode('bad data')
+foo['stuff'] = 5
+
+bar = decode('also bad')
+bar['meep'] = 1
+
+print('foo:', foo)
+print('bar:', bar)
+
+# 기본인수는 모듈 로드 시점에 함수 정의과정에서 한번만 평가된다. 그러므로 동적으로 값을 넘길때는 None 을 사용해야 한다
 
 
